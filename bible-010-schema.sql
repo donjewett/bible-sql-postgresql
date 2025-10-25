56daf7904761e26d7e8ad89c1e18a6fd44bd33a7
Don Jewett <32474612+donjewett@users.noreply.github.com>
2025-10-25 05:53:58 -0700

    Version: 2025.10.24

---
+++ bible-010-schema.sql
@@ -0,0 +1,315 @@
+/* *************************************************************************
+Bible Database: PostgreSQL, by Don Jewett
+https://github.com/donjewett/bible-sql-postgresql
+
+bible-010-schema.sql
+Version: 2025.10.24
+
+MIT License
+
+Copyright (c) 2025 by Don Jewett
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+************************************************************************* */
+
+-- -------------------------------------------------------------------------
+-- Languages
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Languages (
+	Id char(3) NOT NULL,
+	Name varchar(16) NOT NULL,
+	HtmlCode char(2) NOT NULL,
+	IsAncient boolean NOT NULL DEFAULT false,
+ 	CONSTRAINT PK_Language PRIMARY KEY (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Canons
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Canons (
+	Id int NOT NULL,
+	Code char(2) NOT NULL,
+	Name varchar(24) NOT NULL,
+	LanguageId varchar(3) NOT NULL,
+	CONSTRAINT PK_Canons PRIMARY KEY (Id),
+	CONSTRAINT FK_Canons_Languages FOREIGN KEY (LanguageId) REFERENCES Languages (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Sections
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Sections (
+	Id int NOT NULL,
+	Name varchar(16) NOT NULL,
+	CanonId int NOT NULL,
+	CONSTRAINT PK_Sections PRIMARY KEY (Id),
+	CONSTRAINT FK_Sections_Canons FOREIGN KEY (CanonId) REFERENCES Canons (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Books
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Books (
+	Id int NOT NULL,
+	Code varchar(5) NOT NULL,
+	Abbrev varchar(5) NOT NULL,
+	Name varchar(16) NOT NULL,
+	Book smallint NOT NULL,
+	CanonId int NOT NULL, -- denormalized
+	SectionId int NOT NULL,
+	IsSectionEnd boolean NOT NULL DEFAULT false,
+	ChapterCount smallint NOT NULL,
+	OsisCode varchar(6) NOT NULL,
+	Paratext char(3) NOT NULL,
+	CONSTRAINT PK_Books PRIMARY KEY (Id),
+	CONSTRAINT FK_Books_Canons FOREIGN KEY (CanonId) REFERENCES Canons (Id),
+	CONSTRAINT FK_Books_Sections FOREIGN KEY (SectionId) REFERENCES Sections (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- BookNames
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS BookNames (
+	Name varchar(64) NOT NULL,
+	BookId int NOT NULL,
+	CONSTRAINT PK_BookNames PRIMARY KEY (Name),
+	CONSTRAINT FK_BookNames_Books FOREIGN KEY (BookId) REFERENCES Books (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Chapters
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Chapters (
+	Id int NOT NULL,
+	Code varchar(7) NOT NULL,
+	Reference varchar(8) NOT NULL,
+	Chapter smallint NOT NULL,
+	BookId int NOT NULL,
+	IsBookEnd boolean NOT NULL DEFAULT false,
+	VerseCount int NOT NULL,
+	CONSTRAINT PK_Chapters PRIMARY KEY (Id),
+	CONSTRAINT FK_Chapters_Books FOREIGN KEY (BookId) REFERENCES Books (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Verses
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Verses (
+	Id int NOT NULL,
+	Code varchar(16) NOT NULL,
+	OsisCode varchar(12) NOT NULL,
+	Reference varchar(10) NOT NULL,
+	CanonId int NOT NULL, --denormalized
+	SectionId int NOT NULL, --denormalized
+	BookId int NOT NULL, --denormalized
+	ChapterId int NOT NULL,
+	IsChapterEnd boolean NOT NULL DEFAULT false,
+	Book smallint NOT NULL, --denormalized
+	Chapter smallint NOT NULL, --denormalized
+	Verse smallint NOT NULL,
+	CONSTRAINT PK_Verses PRIMARY KEY (Id),
+	CONSTRAINT FK_Verses_Books FOREIGN KEY (BookId) REFERENCES Books (Id),
+	CONSTRAINT FK_Verses_Chapters FOREIGN KEY (ChapterId) REFERENCES Chapters (Id),
+	CONSTRAINT FK_Verses_Canons FOREIGN KEY (CanonId) REFERENCES Canons (Id),
+	CONSTRAINT FK_Verses_Sections FOREIGN KEY (SectionId) REFERENCES Sections (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- GreekTextForms
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS GreekTextForms (
+	Id char(3) NOT NULL,
+	Name varchar(48) NOT NULL,
+	ParentId char(3) NULL,
+	CONSTRAINT PK_GreekTextForms PRIMARY KEY (Id),
+	CONSTRAINT FK_GreekTextForms_GreekTextForms FOREIGN KEY (ParentId) REFERENCES GreekTextForms (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- HebrewTextForms
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS HebrewTextForms (
+	Id char(3) NOT NULL,
+	Name varchar(48) NOT NULL,
+	ParentId char(3) NULL,
+	CONSTRAINT PK_HebrewTextForms PRIMARY KEY (Id),
+	CONSTRAINT FK_HebrewTextForms_GHebrewTextForms FOREIGN KEY (ParentId) REFERENCES HebrewTextForms (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- LicensePermissions
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS LicensePermissions (
+	Id serial NOT NULL,
+	Name varchar(48) NOT NULL,
+	Permissiveness int NOT NULL,
+	CONSTRAINT PK_LicensePermissions PRIMARY KEY (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- LicenseTypes
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS LicenseTypes (
+	Id serial NOT NULL,
+	Name varchar(64) NOT NULL,
+	IsFree boolean NOT NULL DEFAULT false,
+	PermissionId int NULL,
+	CONSTRAINT PK_LicenseType PRIMARY KEY (Id),
+	CONSTRAINT FK_LicenseTypes_LicensePermissions FOREIGN KEY (PermissionId) REFERENCES LicensePermissions (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Versions
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Versions (
+	Id serial NOT NULL,
+	Code varchar(16) NOT NULL,
+	Name varchar(64) NOT NULL,
+	Subtitle varchar(128) NULL,
+	LanguageId char(3) NOT NULL,
+	YearPublished smallint NOT NULL,
+	HebrewFormId char(3) NULL,
+	GreekFormId char(3) NULL,
+	ParentId int NULL,
+	LicenseTypeId int NULL,
+	ReadingLevel decimal(4, 2) NULL,
+	CONSTRAINT PK_Versions PRIMARY KEY (Id),
+	CONSTRAINT FK_Versions_Languages FOREIGN KEY (LanguageId) REFERENCES Languages (Id),
+	CONSTRAINT FK_Versions_Versions FOREIGN KEY (ParentId) REFERENCES Versions (Id),
+	CONSTRAINT FK_Versions_GreekTextForms FOREIGN KEY (GreekFormId) REFERENCES GreekTextForms (Id),
+	CONSTRAINT FK_Versions_HebrewTextForms FOREIGN KEY (HebrewFormId) REFERENCES HebrewTextForms (Id),
+	CONSTRAINT FK_Versions_LicenseTypes FOREIGN KEY (LicenseTypeId) REFERENCES LicenseTypes (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Revisions
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Revisions (
+	Id serial NOT NULL,
+	Code varchar(16) NOT NULL,
+	VersionId int NOT NULL,
+	YearPublished smallint NOT NULL,
+	Subtitle varchar(128) NULL,
+	CONSTRAINT PK_Revisions PRIMARY KEY (Id),
+	CONSTRAINT FK_Revisions_Versions FOREIGN KEY (VersionId) REFERENCES Versions (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Sites
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Sites (
+	Id serial NOT NULL,
+	Name varchar(64) NOT NULL,
+	Url varchar(255) NULL,
+	CONSTRAINT PK_Sites PRIMARY KEY (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- ResourceTypes
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS ResourceTypes (
+	Id varchar(8) NOT NULL,
+	Name varchar(64) NOT NULL,
+	CONSTRAINT PK_ResourceTypes PRIMARY KEY (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Resources
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Resources (
+	Id serial NOT NULL,
+	ResourceTypeId varchar(8) NOT NULL,
+	VersionId int NOT NULL,
+	RevisionId int NULL,
+	Url varchar(255) NULL,
+	IsOfficial boolean NOT NULL DEFAULT false,
+	SiteId int NULL,
+	CONSTRAINT PK_Resources PRIMARY KEY (Id),
+	CONSTRAINT FK_Resources_Versions FOREIGN KEY (VersionId) REFERENCES Versions (Id),
+	CONSTRAINT FK_Resources_Revisions FOREIGN KEY (RevisionId) REFERENCES Revisions (Id),
+	CONSTRAINT FK_Resources_ResourceTypes FOREIGN KEY (ResourceTypeId) REFERENCES ResourceTypes (Id),
+	CONSTRAINT FK_Resources_Sites FOREIGN KEY (SiteId) REFERENCES Sites (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- Bibles
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Bibles (
+	Id serial NOT NULL,
+	Code varchar(16) NOT NULL,
+	Name varchar(64) NOT NULL,
+	Subtitle varchar(128) NULL,
+	VersionId int NOT NULL,
+	RevisionId int NULL,
+	YearPublished smallint NULL,
+	TextFormat varchar(6) NOT NULL DEFAULT ('txt'),
+	SourceId int NULL,
+	CONSTRAINT PK_Bibles PRIMARY KEY (Id),
+	CONSTRAINT FK_Bibles_Versions FOREIGN KEY (VersionId) REFERENCES Versions (Id),
+	CONSTRAINT FK_Bibles_Revisions FOREIGN KEY (RevisionId) REFERENCES Revisions (Id),
+	CONSTRAINT FK_Bibles_Resources FOREIGN KEY (SourceId) REFERENCES Resources (Id)
+);
+
+-- -------------------------------------------------------------------------
+-- BibleVerses
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS BibleVerses (
+	Id serial NOT NULL,
+	BibleId int NOT NULL,
+	VerseId int NOT NULL,
+	Markup text NOT NULL,
+	PreMarkup varchar(255) NULL,
+	PostMarkup varchar(255) NULL,
+	Notes varchar(255) NULL,
+	CONSTRAINT PK_BibleVerses PRIMARY KEY (Id),
+	CONSTRAINT FK_BibleVerses_Bibles FOREIGN KEY (BibleId) REFERENCES Bibles (Id),
+	CONSTRAINT FK_BibleVerses_Verses FOREIGN KEY (VerseId) REFERENCES Verses (Id)
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS UQ_BibleVerses_Version_Verse ON BibleVerses
+(
+	BibleId ASC,
+	VerseId ASC
+);
+
+-- -------------------------------------------------------------------------
+-- VersionNotes
+-- -------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS VersionNotes (
+	Id serial NOT NULL,
+	VersionId int NOT NULL,
+	RevisionId int NULL,
+	BibleId int NULL,
+	CanonId int NULL,
+	BookId int NULL,
+	ChapterId int NULL,
+	VerseId int NULL,
+	Note text NOT NULL,
+	Label varchar(64) NULL,
+	Ranking int NOT NULL DEFAULT 0,
+	CONSTRAINT PK_VersionNotes PRIMARY KEY (Id),
+	CONSTRAINT FK_VersionNotes_Versions FOREIGN KEY (VersionId) REFERENCES Versions (Id),
+	CONSTRAINT FK_VersionNotes_Revisions FOREIGN KEY (RevisionId) REFERENCES Revisions (Id),
+	CONSTRAINT FK_VersionNotes_Bibles FOREIGN KEY (BibleId) REFERENCES Bibles (Id),
+	CONSTRAINT FK_VersionNotes_Canons FOREIGN KEY (CanonId) REFERENCES Canons (Id),
+	CONSTRAINT FK_VersionNotes_Books FOREIGN KEY (CanonId) REFERENCES Books (Id),
+	CONSTRAINT FK_VersionNotes_Chapters FOREIGN KEY (ChapterId) REFERENCES Chapters (Id),
+	CONSTRAINT FK_VersionNotes_Verses FOREIGN KEY (VerseId) REFERENCES Verses (Id)
+);
---
+++ bible-015-descriptions.sql
@@ -0,0 +1,235 @@
+/* *************************************************************************
+Bible Database: PostgreSQL, by Don Jewett
+https://github.com/donjewett/bible-sql-postgresql
+
+bible-015-descriptions.sql
+Version: 2025.10.24
+
+MIT License
+
+Copyright (c) 2025 by Don Jewett
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+************************************************************************* */
+
+----------------------------------------------------------------------------
+-- Languages
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Languages.Id IS 			'Three character ISO 693-1 code';
+COMMENT ON COLUMN Languages.Name IS 		'Name of the Language in English';
+COMMENT ON COLUMN Languages.HtmlCode IS 	'Two character html code for Language';
+COMMENT ON COLUMN Languages.IsAncient IS 	'This language or dialect has been extinct since ancient times';
+
+
+----------------------------------------------------------------------------
+-- Canons
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Canons.Id IS 			'Canon Id following bible-sql numbering scheme';
+COMMENT ON COLUMN Canons.Code IS 		'Short code following Protestant tradition';
+COMMENT ON COLUMN Canons.Name IS 		'Name following Protestant tradition';
+COMMENT ON COLUMN Canons.LanguageId IS 	'Primary Language of the Canon';
+
+
+----------------------------------------------------------------------------
+-- Sections
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Sections.Id IS 		'Section Id following bible-sql numbering scheme';
+COMMENT ON COLUMN Sections.Name IS 		'Name of the Section following Protestant tradition';
+COMMENT ON COLUMN Sections.CanonId IS 	'Canon of the Bible Section';
+
+
+----------------------------------------------------------------------------
+-- Books
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Books.Id IS 				'Book Id following bible-sql numbering scheme';
+COMMENT ON COLUMN Books.Code IS 			'Short url-friendly lowercase Code for Book';
+COMMENT ON COLUMN Books.Abbrev IS 			'Short Proper case abbreviation for Book';
+COMMENT ON COLUMN Books.Name IS 			'Name of Book following Protestant tradition';
+COMMENT ON COLUMN Books.Book IS 			'Index of Book following Protestant order';
+COMMENT ON COLUMN Books.CanonId IS 			'Canon of Book following Protestant tradition';
+COMMENT ON COLUMN Books.SectionId IS 		'Section of Book following Protestant tradition';
+COMMENT ON COLUMN Books.IsSectionEnd IS 	'Is the final Book in the Section';
+COMMENT ON COLUMN Books.ChapterCount IS 	'Count of chapters in this book following Protestant tradition';
+COMMENT ON COLUMN Books.OsisCode IS 		'Osis code for the Book';
+COMMENT ON COLUMN Books.Paratext IS 		'Paratext code for the Book';
+
+
+----------------------------------------------------------------------------
+-- BookNames
+----------------------------------------------------------------------------
+COMMENT ON COLUMN BookNames.Name IS 		'Altername name or code for Book';
+COMMENT ON COLUMN BookNames.BookId IS 		'Book Id following bible-sql numbering scheme';
+
+
+----------------------------------------------------------------------------
+-- Chapters
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Chapters.Id IS 			'Chapter Id following bible-sql numbering scheme';
+COMMENT ON COLUMN Chapters.Code IS 			'Short, url-friendly lowercase Code for chapter';
+COMMENT ON COLUMN Chapters.Reference IS 	'Human readable reference using Book Abbrev and Chapter number';
+COMMENT ON COLUMN Chapters.Chapter IS 		'Chapter number';
+COMMENT ON COLUMN Chapters.BookId IS 		'Book of the Chapter';
+COMMENT ON COLUMN Chapters.IsBookEnd IS 	'Is the final Chapter in the Book';
+COMMENT ON COLUMN Chapters.VerseCount IS 	'Count of verses in the Chapter';
+
+
+----------------------------------------------------------------------------
+-- Verses
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Verses.Id IS 				'Verse Id following bible-sql numbering scheme';
+COMMENT ON COLUMN Verses.Code IS 			'Short, url-friendly lowercase Code for verse';
+COMMENT ON COLUMN Verses.OsisCode IS 		'Osis code for the Verse';
+COMMENT ON COLUMN Verses.Reference IS 		'Human readable reference using Book Abbrev Chapter:Verse';
+COMMENT ON COLUMN Verses.CanonId IS 		'Canon of the Verse'; --denormalized
+COMMENT ON COLUMN Verses.SectionId IS 		'Section of the Verse'; --denormalized
+COMMENT ON COLUMN Verses.BookId IS 			'Book of the Verse'; --denormalized
+COMMENT ON COLUMN Verses.ChapterId IS 		'Chapter of the Verse';
+COMMENT ON COLUMN Verses.IsChapterEnd IS 	'Is the final Verse in the Chapter';
+COMMENT ON COLUMN Verses.Book IS 			'Book number'; --denormalized
+COMMENT ON COLUMN Verses.Chapter IS 		'Chapter number'; --denormalized
+COMMENT ON COLUMN Verses.Verse IS 			'Verse number';
+
+
+----------------------------------------------------------------------------
+-- GreekTextForms
+----------------------------------------------------------------------------
+COMMENT ON COLUMN GreekTextForms.Id IS 			'One to three character code for Greek form';
+COMMENT ON COLUMN GreekTextForms.Name IS 		'Name of the Greek form';
+COMMENT ON COLUMN GreekTextForms.ParentId IS 	'The Greek form this one derives from';
+
+
+----------------------------------------------------------------------------
+-- HebrewTextForms
+----------------------------------------------------------------------------
+COMMENT ON COLUMN HebrewTextForms.Id IS 		'One to three character code for Hebrew text';
+COMMENT ON COLUMN HebrewTextForms.Name IS 		'Name of the Hebrew form';
+COMMENT ON COLUMN HebrewTextForms.ParentId IS 	'The Hebrew form this one derives from';
+
+
+----------------------------------------------------------------------------
+-- LicensePermissions
+----------------------------------------------------------------------------
+COMMENT ON COLUMN LicensePermissions.Id IS 				'Auto-incrementing Id';
+COMMENT ON COLUMN LicensePermissions.Name IS 			'Name of the Permission';
+COMMENT ON COLUMN LicensePermissions.Permissiveness IS 	'Permissiveness of license on a scale of 0 to 100';
+
+
+----------------------------------------------------------------------------
+-- LicenseTypes
+----------------------------------------------------------------------------
+COMMENT ON COLUMN LicenseTypes.Id IS 			'Auto-incrementing Id';
+COMMENT ON COLUMN LicenseTypes.Name IS 			'Name of the License Type';
+COMMENT ON COLUMN LicenseTypes.IsFree IS 		'True for licences allowing free quotation -- false for commercial restricting use';
+COMMENT ON COLUMN LicenseTypes.PermissionId IS 	'Permissiveness for License Type';
+
+
+----------------------------------------------------------------------------
+-- Versions
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Versions.Id IS 			'Auto-incrementing Id';
+COMMENT ON COLUMN Versions.Code IS 			'Version Code used for lookups. Must be unique';
+COMMENT ON COLUMN Versions.Name IS 			'Name of the Version';
+COMMENT ON COLUMN Versions.Subtitle IS 		'Optional Subtitle for the version';
+COMMENT ON COLUMN Versions.LanguageId IS 	'Language of the Version';
+COMMENT ON COLUMN Versions.YearPublished IS 'Year first published in entirety';
+COMMENT ON COLUMN Versions.HebrewFormId IS 	'Textual basis for the Old Testament';
+COMMENT ON COLUMN Versions.GreekFormId IS 	'Textual basis for the New Testament';
+COMMENT ON COLUMN Versions.ParentId IS 		'Optional Version this is derived from';
+COMMENT ON COLUMN Versions.LicenseTypeId IS 'Optional License Type';
+COMMENT ON COLUMN Versions.ReadingLevel IS 	'Reading Level using U.S. school grades (8.0 = Eighth Grade)';
+
+
+----------------------------------------------------------------------------
+-- Revisions
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Revisions.Id IS 			'Auto-incrementing Id';
+COMMENT ON COLUMN Revisions.Code IS 			'Revision Code used for lookups. Must be unique';
+COMMENT ON COLUMN Revisions.VersionId IS 	'Version of the Revision';
+COMMENT ON COLUMN Revisions.YearPublished IS 'Year the Revision was first published in its entirety';
+COMMENT ON COLUMN Revisions.Subtitle IS 		'Subtitle for the Revision, esp. if different than Version (i.e. Second Edition)';
+
+
+----------------------------------------------------------------------------
+-- Sites
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Sites.Id IS 	'Auto-incrementing Id';
+COMMENT ON COLUMN Sites.Name IS 'Site Name';
+COMMENT ON COLUMN Sites.Url IS 	'Site Url';
+
+
+----------------------------------------------------------------------------
+-- ResourceTypes
+----------------------------------------------------------------------------
+COMMENT ON COLUMN ResourceTypes.Id IS 	'Eight character code';
+COMMENT ON COLUMN ResourceTypes.Name IS 'Name of the Resource Type';
+
+
+----------------------------------------------------------------------------
+-- Resources
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Resources.Id IS 				'Auto-incrementing Id';
+COMMENT ON COLUMN Resources.ResourceTypeId IS 	'Eight character code for Resource Type';
+COMMENT ON COLUMN Resources.VersionId IS 		'Version of the Resource';
+COMMENT ON COLUMN Resources.RevisionId IS 		'Optional Revision of the Resource';
+COMMENT ON COLUMN Resources.Url IS 				'Source Url for the Resource';
+COMMENT ON COLUMN Resources.IsOfficial IS 		'True if the Resource is the official one (i.e. provided by the publisher)';
+COMMENT ON COLUMN Resources.SiteId IS 			'Site associated with the Resource';
+
+
+----------------------------------------------------------------------------
+-- Bibles
+----------------------------------------------------------------------------
+COMMENT ON COLUMN Bibles.Id IS 				'Auto-incrementing Id';
+COMMENT ON COLUMN Bibles.Code IS 			'Code for lookups. Must be unique';
+COMMENT ON COLUMN Bibles.Name IS 			'Name of this specific Bible';
+COMMENT ON COLUMN Bibles.Subtitle IS 		'Subtitle for this specific Bible';
+COMMENT ON COLUMN Bibles.VersionId IS 		'Version of this Bible';
+COMMENT ON COLUMN Bibles.RevisionId IS 		'Optional Revision of this Bible';
+COMMENT ON COLUMN Bibles.YearPublished IS 	'Year this Bible (or Revision) was published';
+COMMENT ON COLUMN Bibles.TextFormat IS 		'Code for the format of the Content';
+COMMENT ON COLUMN Bibles.SourceId IS 		'Optional source Resource of this Bible';
+
+
+----------------------------------------------------------------------------
+-- BibleVerses
+----------------------------------------------------------------------------
+COMMENT ON COLUMN BibleVerses.Id IS 		'Auto-incrementing Id';
+COMMENT ON COLUMN BibleVerses.BibleId IS 	'The Bible for this Content';
+COMMENT ON COLUMN BibleVerses.VerseId IS 	'The Verse for this Content';
+COMMENT ON COLUMN BibleVerses.Markup IS 	'The Content of the Bible Verse';
+COMMENT ON COLUMN BibleVerses.PreMarkup IS 	'Optional Markup, especially for scribal notes in the manuscripts.';
+COMMENT ON COLUMN BibleVerses.PostMarkup IS 'Optional Markup, especially for scribal notes in the manuscripts.';
+COMMENT ON COLUMN BibleVerses.Notes IS 		'Optional Notes for the Bible Verse';
+
+
+----------------------------------------------------------------------------
+-- VersionNotes
+----------------------------------------------------------------------------
+COMMENT ON COLUMN VersionNotes.Id IS 		'Auto-incrementing Id';
+COMMENT ON COLUMN VersionNotes.VersionId IS 'Version for this Note';
+COMMENT ON COLUMN VersionNotes.RevisionId IS 'Optional Revision for this Note -- for notes specific to an Revision';
+COMMENT ON COLUMN VersionNotes.BibleId IS 	'Optional Bible for this Note -- for notes specific to a Bible';
+COMMENT ON COLUMN VersionNotes.CanonId IS 	'Optional Canon for this Note -- for notes that apply to the Canon as a whole';
+COMMENT ON COLUMN VersionNotes.BookId IS 	'Optional Book for this Note -- for notes that apply to the Book as a whole';
+COMMENT ON COLUMN VersionNotes.ChapterId IS 'Optional Chapter for this Note -- for notes that apply to the Chapter as a whole';
+COMMENT ON COLUMN VersionNotes.VerseId IS 	'Optional Verse for this Note -- for notes that apply to the Verse. Use BibleVerses.Notes for short notes';
+COMMENT ON COLUMN VersionNotes.Note IS 		'Content of the Note';
+COMMENT ON COLUMN VersionNotes.Label IS 	'Optional Label for the Note';
+COMMENT ON COLUMN VersionNotes.Ranking IS 	'Weight (Descending Sort Order) of the Note';
